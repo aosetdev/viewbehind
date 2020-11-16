61e9921e28f0c74c6fe120d55a9004976b43832b
aosetdev <aosett@gmail.com>
2020-11-15 19:36:36 -0500

    update all

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,8 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['@mdi/font/css/materialdesignicons.css'],
+  css: [],
+  // css: ['@mdi/font/css/materialdesignicons.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
